902abfb4efffc85394117aa2e6c8ec089eb0b3ac
Swedlg <swedlg@yandex.ru>
2022-11-18 11:22:44 +0400

    Add 1 Test

---
+++ src/test/java/Test1.java
@@ -0,0 +1,8 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Test1 {
+    void SomeTest(){
+        String str = "Albert";
+        assertEquals("Albert", str);
+    }
+}
